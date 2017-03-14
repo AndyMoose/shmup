1cfcd9437ed8af4a38ddd0b18c2a801d1e29920a
AndyMoose <amoose96@outlook.com>
2017-03-14 16:59:03 -0400

    Set up game camera and added scrolling BG

---
+++ Assets/BGScroller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroller : MonoBehaviour {
+
+    public float scrollSpeed;
+    public float tileSizeX;
+
+    private float newPos;
+    private Vector2 startPos;
+
+    void Start () {
+        startPos = transform.position;
+	}
+	
+	void Update () {
+        newPos = Mathf.Repeat(Time.time * scrollSpeed, tileSizeX);
+        transform.position = startPos + Vector2.left * newPos;
+    }
+}
