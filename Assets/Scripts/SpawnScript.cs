83019127dc064f140b464ad1294a76f87bc5113f
AndyMoose <amoose96@outlook.com>
2017-03-19 19:14:33 -0400

    Added wave spawner and wave system for game (see discord for details)

---
+++ Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+[CreateAssetMenu]
+public class SpawnScript : ScriptableObject
+{
+    public GameObject enemy;
+    public int spawnTime;
+    public Vector2 spawnPoint;
+    public Animator animator;
+}
+
---
+++ Assets/Scripts/Wave.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+[CreateAssetMenu]
+public class Wave : ScriptableObject {
+
+
+    public List<SpawnScript> spawnList;
+
+    public float waveTime;
+
+    void AddNew()
+    {
+        spawnList.Add(new SpawnScript());
+
+    }
+
+    private void Remove(int index)
+    {
+        spawnList.RemoveAt(index);
+    }
+}
